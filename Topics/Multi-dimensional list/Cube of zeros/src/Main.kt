dcbd8f0fdcd7c4de7b5022911cc31aa90da4cfe8
MostlySunny <psannikov.fpi@gmail.com>
2023-01-08 19:43:26 +0500

    perform extra tasks in project

---
+++ Topics/Multi-dimensional list/Cube of zeros/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    //write your code here
+    val thirdDList = mutableListOf(
+        mutableListOf(mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0)),
+        mutableListOf(mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0)),
+        mutableListOf(mutableListOf<Int>(0, 0, 0), mutableListOf<Int>(0, 0, 0), mutableListOf(0, 0, 0)),
+    )
+    println(thirdDList)
+}
